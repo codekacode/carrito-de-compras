b9aa0f370a3b62d446be8b0641003a37cea576a8
Codekacode <karem.quiroz.m@gmail.com>
2021-03-31 13:07:28 -0500

    Add product function

--- app.js
+++ app.js
@@ -12,10 +12,21 @@ function showData(data){
   for(let i=0; i<data.length; i++){
     let product = `<div class="product" id="products">
     <img src=${data[i].img}>
-    <h3>${data[i].name}</h3>
-    <span>${data[i].precio}</span>
+    <h3 class="product__title" id="product__title">${data[i].name}</h3>
+    <span class="product__price" id="product__price">${data[i].precio}</span>
     <a class="add__carrito" href="#" >Anadir al Carrito<i class="product__icon fas fa-cart-plus"></i></a>
     </div>`
     container.innerHTML += product
   }
 }
+
+function addProduct(){
+  let carrito = document.querySelectorAll('a')
+  for(let i=0; i< carrito.length; i++){
+    carrito[i].addEventListener('click', () => {
+      console.log("chick product")
+    })
+  }
+}
+
+addProduct()

2e5b55f245bf3bef3da474f319191b1587d258d3
Codekacode <karem.quiroz.m@gmail.com>
2021-03-31 00:24:48 -0500

    Add api json and  show products on template

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
+const products = document.getElementById('products');
 
+const fetchData = async () => {
+  try {
+    const res = await fetch('store.json');
+    const data = await res.json(); 
+    showProducts(data)
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+fetchData()
+
+function showProducts(data) {
+  data.forEach(product => {
+    console.log(product);
+  })
+}
+
+let carrito = document.querySelectorAll('.add__carrito');
+
+
+for(let i=0; i< carrito.length; i++) {
+  carrito[i].addEventListener('click', () => {
+    carritoItems()
+  });
+}
+
+function carritoItems() {
+  let productsItems = parseInt(localStorage.getItem('hola'));
+  console.log(typeof(productsItems))
+  localStorage.setItem('hola', 3);
+}
